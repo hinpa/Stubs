13e6ff93ea8c0ec76b625216540525bcebe1367f
hinpa <masterz4pl@gmail.com>
2024-08-27 17:34:01 +0000

    Stubs made

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "./stub-lib/lib.hpp"
+
+
+int main()
+{
+    std::cout << add(1,2) << std::endl;
+    std::cout << sub(1,2) << std::endl;
+    std::cout << mul(2,2) << std::endl;
+
+}
